878cbfe321f5817e33f724c06571d36a2ffe83cf
Diogo Soares Peixoto <71359547+Diogo-Peixoto@users.noreply.github.com>
2024-05-24 17:39:48 -0300

    feature: Logic for the specific article route created

--- src/router/articles.ts
+++ src/router/articles.ts
@@ -1,19 +1,27 @@
 import { FastifyInstance } from "fastify"
 import { dataMainArticles } from "../data"
+import { z } from "zod"
 
 export const articlesRoutes = async (app: FastifyInstance) => {
     app.get('/main', (req, res) => {
-        const articles = dataMainArticles.filter((article) => article.rank <= 3).sort()
+        const articles = dataMainArticles.filter((article) => article.rank <= 3).sort((a, b) => a.rank > b.rank ? 1 : -1)
 
         return { articles }
 
     })
 
-    app.get('/secondary', async () => {
-        const articles = dataMainArticles.filter((article) => article.rank > 3).sort()
-
+    app.get('/secondary', async (req, res) => {
+        const articles = dataMainArticles.filter((article) => article.rank > 3).sort((a, b) => a.rank > b.rank ? 1 : -1)
         return { articles }
     })
 
-    app.get('/:id', async () => { })
+    app.get('/:id', async (req, res) => {
+        const getArticleParamsSchema = z.object({ id: z.string().uuid() })
+
+        const { id } = getArticleParamsSchema.parse(req.params)
+
+        const article = dataMainArticles.filter((article) => article.id === id)
+
+        return article
+    })
 }
