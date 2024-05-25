3cae5e0584064e18328d6931a827d82c56f37125
Diogo Soares Peixoto <71359547+Diogo-Peixoto@users.noreply.github.com>
2024-05-25 15:14:05 -0300

    feature: Enabling CORS in the application

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import fastify from "fastify";
 import { articlesRoutes } from "./router/articles";
+import cors from '@fastify/cors'
+
 
 const app = fastify();
 
+app.register(cors)
 app.register(articlesRoutes, { prefix: 'articles' })
 
 app.listen({ port: 3333 }).then(() => console.log("Server Running!"));
