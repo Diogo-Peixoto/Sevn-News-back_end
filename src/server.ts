4a0e4e2933bf73da8de01701b4a2fdf0f64ef9ac
Diogo Soares Peixoto <71359547+Diogo-Peixoto@users.noreply.github.com>
2024-05-26 17:11:25 -0300

    feature: Passing dynamic environment variables to the server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from "fastify";
 import { articlesRoutes } from "./router/articles";
 import cors from '@fastify/cors'
+import 'dotenv/config'
 
 
 const app = fastify();
@@ -8,4 +9,4 @@ const app = fastify();
 app.register(cors)
 app.register(articlesRoutes, { prefix: 'articles' })
 
-app.listen({ port: 3333 }).then(() => console.log("Server Running!"));
+app.listen({ port: parseInt(process.env.PORT!) }).then(() => console.log("Server Running!"));
